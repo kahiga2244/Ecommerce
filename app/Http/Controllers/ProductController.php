51455a2c22b571f7118a762491f447f7fcb77202
kahiga2244 <kahigakamiru.com>
2021-07-21 18:29:06 +0300

    product & seeders

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Session;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    //
-    fuction index()
+    function index()
     {
         return "welcome to product page";
     }
 }
+
+
